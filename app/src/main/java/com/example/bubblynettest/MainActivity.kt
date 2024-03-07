294a9f6b9c1d6a2910fe0347f165050f518a0d93
Thomas Nguyen <thomasmknguyen@gmail.com>
2024-03-07 14:41:30 -0600

    Create add user button

--- app/src/main/java/com/example/bubblynettest/MainActivity.kt
+++ app/src/main/java/com/example/bubblynettest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bubblynettest
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -10,12 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var userAdapter: UserAdapter
+    private lateinit var addUserButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val users = listOf(
+        val users = arrayListOf(
             User(R.mipmap.default_pfp, "john_doe", "johndoe@email.com"),
             User(R.mipmap.default_pfp, "jane_smith", "janesmith@email.com"),
             User(R.mipmap.default_pfp, "bob_jones", "bobjones@email.com"),
@@ -33,5 +35,13 @@ class MainActivity : AppCompatActivity() {
         userAdapter = UserAdapter(users)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = userAdapter
+
+        addUserButton = findViewById((R.id.add_user_button))
+
+        addUserButton.setOnClickListener{
+            var newUser = User(R.mipmap.default_pfp, "thomas_nguyen", "thomasnguyen@email.com")
+            users.add(newUser)
+            userAdapter.notifyItemInserted(users.size - 1)
+        }
     }
 }
